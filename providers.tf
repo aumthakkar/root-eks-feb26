7625f031677dfea852c52c80c7f03f342ab6abf3
aumthakkar <aumthakkar@yahoo.co.uk>
2026-02-12 16:30:40 +0000

    added k8s provider and removed destroy job's dep on apply job

--- providers.tf
+++ providers.tf
@@ -6,12 +6,12 @@ data "aws_eks_cluster_auth" "my_eks_cluster_auth" {
   name = module.eks_cluster_feb26.cluster_name
 }
 
-# provider "kubernetes" {
-#   host                   = module.eks_cluster_jan26.cluster_endpoint
+provider "kubernetes" {
+  host                   = module.eks_cluster_feb26.cluster_endpoint
 
-#   cluster_ca_certificate = base64decode(module.eks_cluster_jan26.cluster_certificate_authority)
-#   token                  = data.aws_eks_cluster_auth.my_eks_cluster_auth.token
-# }
+  cluster_ca_certificate = base64decode(module.eks_cluster_feb26.cluster_certificate_authority)
+  token                  = data.aws_eks_cluster_auth.my_eks_cluster_auth.token
+}
 
 provider "helm" {
   kubernetes = {
